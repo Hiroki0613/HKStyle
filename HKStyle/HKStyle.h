f007103fbb1bf8364a63a3f90d5ceced70720e62
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-12 23:31:15 +0900

    version 1.0.0

---
+++ HKStyle/HKStyle.h
@@ -0,0 +1,19 @@
+//
+//  HKStyle.h
+//  HKStyle
+//
+//  Created by 近藤宏輝 on 2020/03/12.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HKStyle.
+FOUNDATION_EXPORT double HKStyleVersionNumber;
+
+//! Project version string for HKStyle.
+FOUNDATION_EXPORT const unsigned char HKStyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKStyle/PublicHeader.h>
+
+
---
+++ HKStyle/HKTextField.swift
@@ -0,0 +1,9 @@
+//
+//  HKTextField.swift
+//  HKStyle
+//
+//  Created by 近藤宏輝 on 2020/03/12.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
